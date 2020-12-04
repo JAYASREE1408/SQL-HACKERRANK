9de3eb531c5e0814cfb84aa53a235dca0a37af27
JAYASREE T <jayasreet2112@gmail.com>
2020-12-04 21:28:46 +0530

    Update All in one solution.sql

--- All in one solution.sql
+++ All in one solution.sql
@@ -9,7 +9,7 @@ select name from city where population>120000 and countrycode='USA';
 select * from city; 
   
       Query all columns for a city in CITY with the ID 1661. 
-select * from city where ID=1661;
+select * from city where ID=1661; 
 
     Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN
 select * from city where countrycode='JPN';
