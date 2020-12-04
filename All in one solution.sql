41f6b81128dc8d42855d32df26302fd61955ca86
JAYASREE T <jayasreet2112@gmail.com>
2020-12-04 21:29:04 +0530

    Update All in one solution.sql

--- All in one solution.sql
+++ All in one solution.sql
@@ -61,7 +61,7 @@ select name from employee where salary > 2000 && months < 10 order by employee_i
 
   
     Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
-
+ 
     Equilateral: It's a triangle with 3 sides of equal length.
     Isosceles: It's a triangle with 2 sides of equal length.
     Scalene: It's a triangle with 3 sides of differing lengths.
